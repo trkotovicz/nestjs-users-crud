cb6d79e7615bb4ce66d2ae0f9351f99f93c1bdd3
Thais Kotovicz <thais_kotovicz@hotmail.com>
2024-02-07 19:07:45 -0300

    feat: implements a global interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LogInterceptor } from './interceptors/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalInterceptors(new LogInterceptor()); // para usar de maneira global o interceptor
+
   app.enableShutdownHooks();
+
   await app.listen(3000);
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,15 +8,19 @@ import {
   Patch,
   Post,
   Put,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
 import { UpdatePutUserDTO } from './dto/update-put-user.dto';
 import { UserService } from './user.service';
+// import { LogInterceptor } from 'src/interceptors/log.interceptor';
 
+// @UseInterceptors(LogInterceptor) // para utilizar em todos os métodos do controller
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+  // @UseInterceptors(LogInterceptor) para executar em um único método
   @Post()
   async create(@Body() data: CreateUserDTO) {
     return await this.userService.create(data);
